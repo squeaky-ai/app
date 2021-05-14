708c731338e16c619393d5169b2bdabb2e4790c0
Damien Senger <hiwelo@hey.com>
2021-05-14 14:48:33 +0200

    feat(logo): create component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,11 @@ import { NextPage } from 'next';
 import React from 'react';
 import Heading from 'components/Heading';
 import Button from 'components/Button';
+import Logo from 'components/Logo';
 
 const Home: NextPage = () => (
   <>
+    <Logo />
     <Heading>Hello World!</Heading>
     <Heading modSection>Hello World!</Heading>
     <Heading modSubsection>Hello World!</Heading>
