fd8121d13048602f40c07a912a9fa53a9f8f6f24
Lewis Monteith <lewismonteith@gmail.com>
2021-06-15 14:27:47 +0100

    Set the dropdown to close when clicking off

--- src/components/dropdown.tsx
+++ src/components/dropdown.tsx
@@ -8,10 +8,27 @@ interface Props {
 }
 
 export const Dropdown: FC<Props> = ({ button, children }) => {
+  const ref = React.useRef<HTMLDivElement>();
   const [open, setOpen] = React.useState(false);
 
+  const handleClick = (event: MouseEvent) => {
+    const element = event.target as Element;
+
+    if (ref.current && !ref.current.contains(element)) {
+      setOpen(false);
+    }
+  };
+
+  React.useEffect(() => {
+    document.addEventListener('click', handleClick);
+
+    return () => {
+      document.removeEventListener('click', handleClick, true);
+    }
+  }, []);
+
   return (
-    <div className={classnames('dropdown', { open })}>
+    <div ref={ref} className={classnames('dropdown', { open })}>
       <Button onClick={() => setOpen(!open)}>
         {button}
         <i className='arrow ri-arrow-drop-down-line' />
