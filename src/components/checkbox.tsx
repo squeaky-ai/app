14b946ffb049eb481ed4181588fe0eb9fb157e05
Lewis Monteith <lewismonteith@gmail.com>
2021-06-23 11:16:28 +0100

    Make checkboxes keyboard accessible

--- src/components/checkbox.tsx
+++ src/components/checkbox.tsx
@@ -7,12 +7,20 @@ interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   invalid?: boolean;
 }
 
-export const Checkbox: FC<Props> = ({ className, name, children, invalid, ...rest }) => (
-  <Label className={classnames('checkbox', className, { invalid })}>
-    <input type='checkbox' name={name} {...rest} />
-    <span className='check'>
-      <i className='ri-check-line' />
-    </span>
-    <span>{children}</span>
-  </Label>
-);
+export const Checkbox: FC<Props> = ({ className, name, children, invalid, ...rest }) => {
+  const onKeyDown = (event: React.KeyboardEvent<HTMLLabelElement>) => {
+    if (event.code === 'Space') {
+      (event.target as HTMLElement).click();
+    }
+  };
+
+  return (
+    <Label className={classnames('checkbox', className, { invalid })} tabIndex={0} onKeyDown={onKeyDown}>
+      <input type='checkbox' name={name} {...rest} />
+      <span className='check'>
+        <i className='ri-check-line' />
+      </span>
+      <span>{children}</span>
+    </Label>
+  );
+};
