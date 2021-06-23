31596d3009f8da7a24bb43710f49eef38c5f1f39
Lewis Monteith <lewismonteith@gmail.com>
2021-06-23 13:57:35 +0100

    Remove favicon idea as it didnt work

--- src/components/sites/avatar.tsx
+++ src/components/sites/avatar.tsx
@@ -7,19 +7,8 @@ interface Props extends React.HTMLAttributes<HTMLDivElement> {
   site: Site;
 }
 
-export const Avatar: FC<Props> = React.memo(({ className, site, ...rest }) => {
-  const [error, setError] = React.useState<boolean>(false);
-
-  const onError = () => {
-    if (!error) setError(true);
-  };
-
-  return (
-    <div className={classnames('avatar', className)} {...rest}>
-      {error
-        ? <span>{site.name[0]}</span>
-        : <img onError={onError} src={`${site.url}/favicon.ico`} />
-      }
-    </div>  
-  );
-});
+export const Avatar: FC<Props> = ({ className, site, ...rest }) => (
+  <div className={classnames('avatar', className)} {...rest}>
+    <span>{site.name[0]}</span>
+  </div>  
+);
