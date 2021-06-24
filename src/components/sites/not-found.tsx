79c80bf03132563c2e970ca04dbfebfd7168db01
Lewis Monteith <lewismonteith@gmail.com>
2021-06-24 20:51:21 +0100

    Add the new error states

--- src/components/sites/not-found.tsx
+++ src/components/sites/not-found.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import type { FC } from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import { Container } from 'components/container';
 
 export const NotFound: FC = () => (
   <div className='not-found'>
     <Container className='md'>
-      <h3>Not Found</h3>
+      <Image src='/error-state.svg' height={256} width={256} />
+      <h2>404</h2>
+      <p>The page you are looking for cannot be found.</p>
       <Link href='/sites'>
-        <a className='button secondary'>
+        <a className='button primary'>
           Back to sites
         </a>
       </Link>
--- src/components/sites/unauthorized.tsx
+++ src/components/sites/unauthorized.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import type { FC } from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import { Container } from 'components/container';
 
 export const Unauthorized: FC = () => (
   <div className='unauthorized'>
     <Container className='md'>
-      <h3>Unauthorized</h3>
+      <Image src='/error-state.svg' height={256} width={256} />
+      <h2>No Access</h2>
+      <p>You role does not authorize you to view the requested page.</p>
       <Link href='/sites'>
-        <a className='button secondary'>
+        <a className='button primary'>
           Back to sites
         </a>
       </Link>
