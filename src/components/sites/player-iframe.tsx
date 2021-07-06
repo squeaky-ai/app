040a728daa3384c4dfed33c76dcb3315c68f8e75
Lewis Monteith <lewismonteith@gmail.com>
2021-07-06 15:35:43 +0100

    Flush the id map between page views

--- src/components/sites/player-iframe.tsx
+++ src/components/sites/player-iframe.tsx
@@ -48,6 +48,14 @@ export class PlayerIframe extends React.Component<Props> {
 
   private setIframeContents = (event: SnapshotEvent) => {
     const args = JSON.parse(event.snapshot);
+
+    if (event.event === 'initialize') {
+      // The ID map cache needs to be busted between
+      // every pay load or it thinks it's already rendered
+      (this.mirror as any).idMap = {};
+      this.clearPage();
+    }
+
     (this.mirror as any)[event.event].apply(this.mirror, args);
   };
 
@@ -60,7 +68,7 @@ export class PlayerIframe extends React.Component<Props> {
   private handleScrollEvents = (events: ScrollEvent[]) => {
     if (events.length > 0) {
       const { x, y } = events[0];
-      this.document.body.scrollTo({ left: x, top: y, behavior: 'smooth' });
+      this.document.body?.scrollTo({ left: x, top: y, behavior: 'smooth' });
     }
   };
 
@@ -88,7 +96,7 @@ export class PlayerIframe extends React.Component<Props> {
       createElement: (tagName: string) => {
         // Don't display any script tags
         if (tagName === 'SCRIPT') {
-          var node = document.createElement('NO-SCRIPT');
+          const node = document.createElement('NO-SCRIPT');
           node.style.display = 'none';
           return node;
         }
@@ -97,20 +105,28 @@ export class PlayerIframe extends React.Component<Props> {
         // all of the pages relative assets become 
         // absolute
         if (tagName === 'HEAD') {
-          var node = document.createElement('HEAD');
+          const node = document.createElement('HEAD');
           node.appendChild(document.createElement('BASE'));
           (node.firstChild as HTMLLinkElement).href = this.host;
           return node;
         }
 
-        return node;
+        return undefined;
       }
     });
   };
 
+  private setFirstSnapshot = () => {
+    const snapshot = this.props.events.find(e => e.type === 'snapshot' && e.event == 'initialize');
+
+    if (snapshot) {
+      this.setIframeContents(snapshot as SnapshotEvent);
+    }
+  };
+
   private onIframeLoad = () => {
-    this.createMirror();
-    this.clearPage();
+    this.createMirror();   
+    this.setFirstSnapshot();
   };
 
   public render(): JSX.Element {
@@ -122,8 +138,10 @@ export class PlayerIframe extends React.Component<Props> {
 
     return (
       <main id='player'>
-        <div id='cursor' />
-        <iframe id='player' scrolling='no' onLoad={this.onIframeLoad} ref={this.iframe} style={props} />
+        <div className='test' style={props}>
+          <div id='cursor' />
+          <iframe scrolling='no' onLoad={this.onIframeLoad} ref={this.iframe} height='100%' width='100%' />
+        </div>
       </main>
     );
   }
