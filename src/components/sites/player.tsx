01ca0fa0df772da3feab33792edb591571e60ec0
Lewis Monteith <lewismonteith@gmail.com>
2021-07-08 11:57:02 +0100

    Auto zoom to fit on load

--- src/components/sites/player.tsx
+++ src/components/sites/player.tsx
@@ -9,7 +9,26 @@ interface Props {
 }
 
 const Player: FC<Props> = React.memo(({ site }) => {
-  const [state] = usePlayerState();
+  const [state, dispatch] = usePlayerState();
+
+  React.useEffect(() => {
+    const container = document.getElementById('player');
+    const { width, height } = container.getBoundingClientRect();
+    const { viewportX, viewportY } = state.recording;
+
+    let multiplier = 1;
+
+    // Keep shrinking the multiplier until the viewport of the player
+    // window fits inside the bounds of the page
+    while((viewportX * multiplier) > width || (viewportY * multiplier) > height) {
+      multiplier -= .1;
+    }
+
+    // Once JS gets to .4, the precision goes to shit and it ends up
+    // as 0.40000000000000013, so it must be fixed (to a string?! 🤦‍♂️)
+    // and then cast back to a number
+    dispatch({ type: 'zoom', value: Number(multiplier.toFixed(1)) });
+  }, []);
 
   return (
     <PlayerIframe
