70c1b3c4c4f36403b65c8100a9944e02aa95aa8f
Lewis Monteith <lewismonteith@gmail.com>
2021-08-03 13:41:09 +0100

    Add the small menu hovers

--- src/components/app/sidebar.tsx
+++ src/components/app/sidebar.tsx
@@ -35,7 +35,7 @@ export const Sidebar: FC = () => {
         <div className='slider'>
           <div className='nav left'>
             <Link href='/sites'>
-              <a className={classnames('link', { active: path === '/sites' })}>
+              <a className={classnames('link', { active: path === '/sites' })} data-label='All Sites'>
                 <i className='ri-window-line' />
                 <span>All Sites</span>
               </a>
@@ -44,50 +44,50 @@ export const Sidebar: FC = () => {
           </div>
           <div className='nav right'>
             <Link href={`/sites/${siteId}/overview`}>
-              <a className={classnames('link', { active: path === `/sites/${siteId}/overview` })}>
+              <a className={classnames('link', { active: path === `/sites/${siteId}/overview` })} data-label='Overview'>
                 <i className='ri-dashboard-3-line' />
                 <span>Overview</span>
               </a>
             </Link>
             <Link href={`/sites/${siteId}/users`}>
-              <a className={classnames('link', { active: path === `/sites/${siteId}/users` })}>
+              <a className={classnames('link', { active: path === `/sites/${siteId}/users` })} data-label='Users'>
                 <i className='ri-group-line' />
                 <span>Users</span>
               </a>
             </Link>
             <Link href={`/sites/${siteId}/recordings`}>
-              <a className={classnames('link', { active: path === `/sites/${siteId}/recordings` })}>
+              <a className={classnames('link', { active: path === `/sites/${siteId}/recordings` })} data-label='Recordings'>
                 <i className='ri-vidicon-line' />
                 <span>Recordings</span>
               </a>
             </Link>
             <Link href={`/sites/${siteId}/analytics`}>
-              <a className={classnames('link', { active: path === `/sites/${siteId}/analytics` })}>
+              <a className={classnames('link', { active: path === `/sites/${siteId}/analytics` })} data-label='Analytics'>
                 <i className='ri-line-chart-line' />
                 <span>Analytics</span>
               </a>
             </Link>
             <Link href={`/sites/${siteId}/insights`}>
-              <a className={classnames('link', { active: path === `/sites/${siteId}/insights` })}>
+              <a className={classnames('link', { active: path === `/sites/${siteId}/insights` })} data-label='Insights'>
                 <i className='ri-lightbulb-line' />
                 <span>Insights</span>
               </a>
             </Link>
             <div className='divider' />
             <Link href={`/sites/${siteId}/team`}>
-              <a className={classnames('link', { active: path === `/sites/${siteId}/team` })}>
+              <a className={classnames('link', { active: path === `/sites/${siteId}/team` })} data-label='Team'>
                 <i className='ri-group-line' />
                 <span>Team</span>
               </a>
             </Link>
             <Link href={`/sites/${siteId}/settings`}>
-              <a className={classnames('link', { active: path === `/sites/${siteId}/settings` })}>
+              <a className={classnames('link', { active: path === `/sites/${siteId}/settings` })} data-label='Settings'>
                 <i className='ri-settings-3-line' />
                 <span>Settings</span>
               </a>
             </Link>
             <Link href={`/sites/${siteId}/subscription`}>
-              <a className={classnames('link', { active: path === `/sites/${siteId}/subscription` })}>
+              <a className={classnames('link', { active: path === `/sites/${siteId}/subscription` })} data-label='Subscriptions'>
                 <i className='ri-bank-card-2-line' />
                 <span>Subscriptions</span>
               </a>
@@ -98,7 +98,7 @@ export const Sidebar: FC = () => {
         </div>
       </menu>
       <footer>
-        <Button className='link' onClick={toggleOpen}>
+        <Button className='link' onClick={toggleOpen} data-label={open ? 'Expand' : 'Collapse'}>
           <i className='ri-arrow-right-line' />
           <span>Collapse</span>
         </Button>
