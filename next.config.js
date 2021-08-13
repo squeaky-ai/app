534659db7782f40544db0cb70102bc980adf6b52
Lewis Monteith <lewismonteith@gmail.com>
2021-08-13 14:22:40 +0100

    Make the column headings darker

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
-const { API_HOST } = process.env;
+const { API_HOST, NODE_ENV } = process.env;
 
 module.exports = {
   publicRuntimeConfig: {
     apiHost: API_HOST || 'http://localhost:4000'
+  },
+  async rewrites() {
+    return NODE_ENV !== 'production' ? [
+      {
+        source: '/api/:slug*',
+        destination: 'http://localhost:4000/api/:slug*'
+      }
+    ] : [];
   }
 };
