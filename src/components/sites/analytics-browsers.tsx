28f5e68e0356eb851b32911e9912cfde5deebc5a
Lewis Monteith <lewismonteith@gmail.com>
2021-08-13 20:39:40 +0100

    Attempt to make the browser percentages nicer

--- src/components/sites/analytics-browsers.tsx
+++ src/components/sites/analytics-browsers.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import type { FC } from 'react';
-import { sum, orderBy } from 'lodash';
+import { sum, orderBy, first } from 'lodash';
 import { Browser } from 'components/browser';
 import { AnalyticsBrowser } from 'types/analytics';
 
@@ -10,16 +10,21 @@ interface Props {
 
 export const AnalyticsBrowsers: FC<Props> = ({ browsers }) => {
   const total = sum(browsers.map(b => b.count));
+  const ordered = orderBy(browsers, 'count', 'desc');
 
-  const percentage = (count: number) => Math.round((count / total) * 100);
+  const percentage = (count: number) => total ? Math.round((count / total) * 100) : 0;
+
+  const offset = 100 - percentage(first(ordered)?.count || 0);
+
+  const offsettedPercentage = (count: number) => offset + percentage(count);
 
   return (
     <ul className='analytics-browsers'>
-      {orderBy(browsers, 'count', 'desc').map(browser => (
+      {ordered.map(browser => (
         <li key={browser.name}>
           <Browser name={browser.name} height={32} width={32} />
           <div className='contents'>
-            <div className='percentage' style={{ width: `${percentage(browser.count)}%` }} />
+            <div className='percentage' style={{ width: `${offsettedPercentage(browser.count)}%` }} />
             <p><b>{percentage(browser.count)}%</b> {browser.name}</p>
           </div>
         </li>
