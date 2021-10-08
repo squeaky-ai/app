758a7314f0eafe86cd5e002a143483c2102d0f7a
Lewis Monteith <lewismonteith@gmail.com>
2021-10-08 15:48:05 +0100

    Use a boring spinner because chris thinks the old one is too cheesy but I think it is amazing

--- src/components/spinner.tsx
+++ src/components/spinner.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import type { FC } from 'react';
-import Image from 'next/image';
 import classnames from 'classnames';
-import { BASE_PATH } from 'data/common/constants';
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {}
 
 export const Spinner: FC<Props> = ({ className, ...rest }) => (
   <div className={classnames('spinner', className)} {...rest}>
-    <Image 
-      src={`${BASE_PATH}/spinner.svg`} 
-      height={120} 
-      width={120} 
-      alt='Loading spinner' 
-    />
+    <div className='icon'>
+      <i className='ri-loader-4-line' />
+    </div>
   </div>
 );
