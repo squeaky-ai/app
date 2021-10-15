86b2e0a86c6b8ff6b50bd51c34af1763f9943b65
Lewis Monteith <lewismonteith@gmail.com>
2021-10-15 11:21:57 +0100

    Fix 404 from visitors page

--- src/pages/sites/[site_id]/visitors/index.tsx
+++ src/pages/sites/[site_id]/visitors/index.tsx
@@ -108,7 +108,7 @@ const SitesVisitors: NextPage<ServerSideProps> = ({ user }) => {
                   title='Creating Visitor Records'
                   body={
                     <>
-                      <p>New to Squeaky? Please <Link href={`/sites/${site.id}/settings?tab=code`}><a>install your tracking code</a></Link> to begin recording user sessions for your website or web app.</p>
+                      <p>New to Squeaky? Please <Link href={`/sites/${site.id}/settings/tracking-code`}><a>install your tracking code</a></Link> to begin recording user sessions for your website or web app.</p>
                       <p>If you have only recently installed or updated your tracking code it may take up to an hour before user records become available.</p>
                     </>
                   }
