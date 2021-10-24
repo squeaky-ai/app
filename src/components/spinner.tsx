914055bf20fa1057fc5b50e5c8599f666d91ea97
Lewis Monteith <lewismonteith@gmail.com>
2021-10-24 10:57:28 +0100

    Add extra spinner state

--- src/components/spinner.tsx
+++ src/components/spinner.tsx
@@ -4,10 +4,25 @@ import classnames from 'classnames';
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {}
 
-export const Spinner: FC<Props> = ({ className, ...rest }) => (
-  <div className={classnames('spinner', className)} {...rest}>
-    <div className='icon'>
-      <i className='ri-loader-4-line' />
+export const Spinner: FC<Props> = ({ className, ...rest }) => {
+  let timeout: NodeJS.Timeout;
+
+  const [showExtra, setShowExtra] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    timeout = setTimeout(() => {
+      setShowExtra(true);
+    }, 5000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return (
+    <div className={classnames('spinner', className)} {...rest}>
+      <div className='icon'>
+        <i className='ri-loader-4-line' />
+      </div>
+      {showExtra && <p>Still working on it...</p>}
     </div>
-  </div>
-);
+  );
+};
