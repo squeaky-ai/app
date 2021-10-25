c859c170238802d0e3dddcc7c96c00c21662f2ae
Lewis Monteith <lewismonteith@gmail.com>
2021-10-25 11:50:39 +0100

    Add a bar chart for the widths

--- src/components/sites/analytics-screen-widths.tsx
+++ src/components/sites/analytics-screen-widths.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import type { FC } from 'react';
-import { min, max } from 'lodash';
+import { min, max, groupBy } from 'lodash';
+import { ResponsiveContainer, Tooltip, TooltipProps, BarChart, Bar, XAxis } from 'recharts';
 import { Card } from 'components/card';
 import { average, frequent } from 'lib/maths';
 
@@ -9,6 +10,24 @@ interface Props {
 }
 
 export const AnalyticsScreenWidths: FC<Props> = ({ dimensions }) => {
+  const CustomTooltip: FC<TooltipProps<any, any>> = ({ active, payload, label }) => {
+    if (!active || !payload || payload?.length < 1) return null;
+  
+    return (
+      <div className='custom-tooltip'>
+        <p className='width'>Width: {label}px</p>
+        <p className='total-visitors'>{payload[0].value} Total Visitors</p>
+      </div>
+    );
+  };
+
+  const groups = groupBy(dimensions);
+
+  const data = Object.values(groups).map(counts => ({ 
+    count: counts.length,
+    width: counts[0],
+  }));
+
   return (
     <Card>
       <h4>
@@ -16,6 +35,19 @@ export const AnalyticsScreenWidths: FC<Props> = ({ dimensions }) => {
         <i className='ri-arrow-left-line' />
         <i className='ri-arrow-right-line' />
       </h4>
+      <div className='chart-wrapper'>
+        <ResponsiveContainer>
+          <BarChart data={data} height={150}>
+            <XAxis dataKey='width' hide />
+            <Bar 
+              dataKey='count' 
+              fill='#4097E8'
+              barSize={4}
+            />
+            <Tooltip cursor={false} content={<CustomTooltip />} />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
       <div className='items'>
         <div className='item'>
           <p>Smallest</p>
