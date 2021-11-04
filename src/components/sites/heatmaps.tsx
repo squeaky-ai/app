ee629b5aa745273eb85b9e954b28c3462b66db6d
Lewis Monteith <lewismonteith@gmail.com>
2021-11-04 15:54:52 +0000

    Add additional empty state for heatmaps

--- src/components/sites/heatmaps.tsx
+++ src/components/sites/heatmaps.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import classnames from 'classnames';
 import type { FC } from 'react';
+import Image from 'next/image';
+import classnames from 'classnames';
 import { Button } from 'components/button';
 import { Spinner } from 'components/spinner';
+import { Container } from 'components/container';
 import { HeatmapsClicks } from 'components/sites/heatmaps-clicks';
 import { HeatmapsScrolls } from 'components/sites/heatmaps-scrolls';
 import { HeatmapsPages } from 'components/sites/heatmaps-pages';
@@ -10,6 +12,7 @@ import { HeatmapsPeriods } from 'components/sites/heatmaps-periods';
 import { HeatmapsPage } from 'components/sites/heatmaps-page';
 import { useHeatmaps } from 'hooks/use-heatmaps';
 import { getDateRange, TimePeriod } from 'lib/dates';
+import { BASE_PATH } from 'data/common/constants';
 import type { HeatmapsDevice, HeatmapsType } from 'types/heatmaps';
 
 interface Props {
@@ -26,12 +29,14 @@ export const Heatmaps: FC<Props> = ({ page, pages, period, setPage, setPeriod })
 
   const { loading, heatmaps } = useHeatmaps({ page, device, type, range: getDateRange(period) });
 
+  const hasData = !!heatmaps.recordingId;
+
   if (loading) {
     return <Spinner />;
   }
 
   return (
-    <div className='heatmaps-grid'>
+    <div className={classnames('heatmaps-grid', { empty: !hasData })}>
       <div className='options'>
         <div className='left'>
           <HeatmapsPages page={page} pages={pages} setPage={setPage} />
@@ -60,19 +65,29 @@ export const Heatmaps: FC<Props> = ({ page, pages, period, setPage, setPeriod })
           </div>
         </div>
       </div>
-      <div className='content'>
-        {heatmaps.recordingId && (
-          <HeatmapsPage 
-            page={page}
-            recordingId={heatmaps.recordingId} 
-            items={heatmaps.items} 
-          />
-        )}
-      </div>
-      <div className='data'>
-        {type === 'Click' && <HeatmapsClicks items={heatmaps.items} />}
-        {type === 'Scroll' && <HeatmapsScrolls items={heatmaps.items} />}
-      </div>
+      {hasData && (
+        <>
+          <div className='content'>
+            <HeatmapsPage 
+              page={page}
+              recordingId={heatmaps.recordingId} 
+              items={heatmaps.items} 
+            />
+          </div>
+          <div className='data'>
+            {type === 'Click' && <HeatmapsClicks items={heatmaps.items} />}
+            {type === 'Scroll' && <HeatmapsScrolls items={heatmaps.items} />}
+          </div>
+        </>
+      )}
+      {!hasData && (
+        <Container className='xl centered empty-state show'>
+          <div className='empty-state-contents'>
+            <Image src={`${BASE_PATH}/empty-state-2.svg`} height={240} width={320} alt='Illustration to represent the empty recordings page' />
+            <h4>There is no heatmap data available for your chosen page, period or device</h4>
+          </div>
+        </Container>
+      )}
     </div>
   );
 };
