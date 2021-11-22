fcb2921a3115fe47150f1bdd59f66b4c8170044c
Lewis Monteith <lewismonteith@gmail.com>
2021-11-22 10:30:30 +0000

    Use offsetted percentages for replies

--- src/components/sites/feedback/sentiment-replies.tsx
+++ src/components/sites/feedback/sentiment-replies.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import type { FC } from 'react';
-import { groupBy } from 'lodash';
+import { groupBy, first } from 'lodash';
 import { EMOJIS } from 'data/sentiment/constants';
 import { percentage } from 'lib/maths';
 import type { SentimentReplies as ISentimentReplies } from 'types/sentiment';
@@ -16,6 +16,17 @@ export const SentimentReplies: FC<Props> = ({ replies }) => {
 
   const getPercentage = (num: number) => percentage(replies.total, getCount(num));
 
+  const biggest = first(Object.entries(groups).sort(([, value]) => value.length)[0]);
+
+  const offset = 100 - getPercentage(Number(biggest) || 0);
+
+  const offsettedPercentage = (count: number) => {
+    const percent = getPercentage(count);
+    const value = offset + percent;
+
+    return percent === 0 ? 1 : value;
+  };
+
   return (
     <div className='sentiment-replies'>
       <ul>
@@ -24,7 +35,7 @@ export const SentimentReplies: FC<Props> = ({ replies }) => {
             {EMOJIS[4]}
           </div>
           <div className='score'>
-            <div className='percentage' style={{ width: `${getPercentage(4) || 1}%` }} />
+            <div className='percentage' style={{ width: `${offsettedPercentage(4)}%` }} />
             <p><b>{getCount(4)}</b> <span>({getPercentage(4)}%)</span></p>
           </div>
         </li>
@@ -33,7 +44,7 @@ export const SentimentReplies: FC<Props> = ({ replies }) => {
             {EMOJIS[3]}
           </div>
           <div className='score'>
-            <div className='percentage' style={{ width: `${getPercentage(3) || 1}%` }} />
+            <div className='percentage' style={{ width: `${offsettedPercentage(3)}%` }} />
             <p><b>{getCount(3)}</b> <span>({getPercentage(3)}%)</span></p>
           </div>
         </li>
@@ -42,7 +53,7 @@ export const SentimentReplies: FC<Props> = ({ replies }) => {
             {EMOJIS[2]}
           </div>
           <div className='score'>
-            <div className='percentage' style={{ width: `${getPercentage(2) || 1}%` }} />
+            <div className='percentage' style={{ width: `${offsettedPercentage(2)}%` }} />
             <p><b>{getCount(2)}</b> <span>({getPercentage(2)}%)</span></p>
           </div>
         </li>
@@ -51,7 +62,7 @@ export const SentimentReplies: FC<Props> = ({ replies }) => {
             {EMOJIS[1]}
           </div>
           <div className='score'>
-            <div className='percentage' style={{ width: `${getPercentage(1) || 1}%` }} />
+            <div className='percentage' style={{ width: `${offsettedPercentage(1)}%` }} />
             <p><b>{getCount(1)}</b> <span>({getPercentage(1)}%)</span></p>
           </div>
         </li>
@@ -60,7 +71,7 @@ export const SentimentReplies: FC<Props> = ({ replies }) => {
             {EMOJIS[0]}
           </div>
           <div className='score'>
-            <div className='percentage' style={{ width: `${getPercentage(0) || 1}%` }} />
+            <div className='percentage' style={{ width: `${offsettedPercentage(0)}%` }} />
             <p><b>{getCount(0)}</b> <span>({getPercentage(0)}%)</span></p>
           </div>
         </li>
