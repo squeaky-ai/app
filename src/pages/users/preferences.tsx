cc0a95f2043322191c5cabca2b2146c41f0fc8e7
Lewis Monteith <lewismonteith@gmail.com>
2021-12-29 17:42:26 +0000

    Fix url flash

--- src/pages/users/preferences.tsx
+++ src/pages/users/preferences.tsx
@@ -5,7 +5,6 @@ import { Container } from 'components/container';
 import { Tabs } from 'components/users/tabs';
 import { Main } from 'components/main';
 import { Toggle } from 'components/toggle';
-import { Spinner } from 'components/spinner';
 import { Error } from 'components/error';
 import { ServerSideProps, getServerSideProps } from 'lib/auth';
 import { updateUserCommunication } from 'lib/api/graphql';
@@ -16,7 +15,7 @@ import { UsersCommunication } from 'types/graphql';
 const UsersPreferences: NextPage<ServerSideProps> = ({ user }) => {
   const toasts = useToasts();
 
-  const { communication, loading, error } = useCommunication();
+  const { communication, error, loading } = useCommunication();
 
   const handleToggle = (type: keyof UsersCommunication) => {
     return async (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -36,10 +35,6 @@ const UsersPreferences: NextPage<ServerSideProps> = ({ user }) => {
     return <Error />;
   }
 
-  if (loading) {
-    return <Spinner />;
-  }
-
   return (
     <>
       <Head>
@@ -53,46 +48,48 @@ const UsersPreferences: NextPage<ServerSideProps> = ({ user }) => {
 
         <p>To manage your communication preferences, simple choose from the options below:</p>
 
-        <Container className='md'>
-          <ul className='email-options'>
-            <li>
-              <Toggle checked={communication.onboardingEmail} onChange={handleToggle('onboardingEmail')}>
-                Onboarding
-              </Toggle>
-              <p>In your first week using Squeaky we&apos;ll sending you 4-6 emails with tips on how to get set up and start making the most of the data you&apos;re capturing.</p>
-            </li>
-            <li>
-              <Toggle checked={communication.weeklyReviewEmail} onChange={handleToggle('weeklyReviewEmail')}>
-                Weekly Review
-              </Toggle>
-              <p>Receive 1 email per week that contains a snapshot of your key analytics data and user feedback from the past week.</p>
-            </li>
-            <li>
-              <Toggle checked={communication.monthlyReviewEmail} onChange={handleToggle('monthlyReviewEmail')}>
-                Monthly Review
-              </Toggle>
-              <p>Receive 1 email per month that contains a snapshot of your key analytics data and user feedback from the past month.</p>
-            </li>
-            <li>
-              <Toggle checked={communication.productUpdatesEmail} onChange={handleToggle('productUpdatesEmail')}>
-                Product Updates
-              </Toggle>
-              <p>Receive an email from Squeaky when we announce major updates to our product such as new features and functionality. We update the app daily, but we aim to limit Product Update emails to no more than once per month.</p>
-            </li>
-            <li>
-              <Toggle checked={communication.marketingAndSpecialOffersEmail} onChange={handleToggle('marketingAndSpecialOffersEmail')}>
-                Marketing &amp; Special Offers
-              </Toggle>
-              <p>No more than 3-5 times per year we like to share special offers and other marketing related announcements about the great things Squeaky can do for your business.</p>
-            </li>
-            <li>
-              <Toggle checked={communication.knowledgeSharingEmail} onChange={handleToggle('knowledgeSharingEmail')}>
-                Knowledge Sharing
-              </Toggle>
-              <p>Receive one email per quarter containing links to the top content that Squeaky has published that quarter. Topics include Product, UX, Marketing, Conversion and Customer Success.</p>
-            </li>
-          </ul>
-        </Container>
+        {!loading && (
+          <Container className='md'>
+            <ul className='email-options'>
+              <li>
+                <Toggle checked={communication.onboardingEmail} onChange={handleToggle('onboardingEmail')}>
+                  Onboarding
+                </Toggle>
+                <p>In your first week using Squeaky we&apos;ll sending you 4-6 emails with tips on how to get set up and start making the most of the data you&apos;re capturing.</p>
+              </li>
+              <li>
+                <Toggle checked={communication.weeklyReviewEmail} onChange={handleToggle('weeklyReviewEmail')}>
+                  Weekly Review
+                </Toggle>
+                <p>Receive 1 email per week that contains a snapshot of your key analytics data and user feedback from the past week.</p>
+              </li>
+              <li>
+                <Toggle checked={communication.monthlyReviewEmail} onChange={handleToggle('monthlyReviewEmail')}>
+                  Monthly Review
+                </Toggle>
+                <p>Receive 1 email per month that contains a snapshot of your key analytics data and user feedback from the past month.</p>
+              </li>
+              <li>
+                <Toggle checked={communication.productUpdatesEmail} onChange={handleToggle('productUpdatesEmail')}>
+                  Product Updates
+                </Toggle>
+                <p>Receive an email from Squeaky when we announce major updates to our product such as new features and functionality. We update the app daily, but we aim to limit Product Update emails to no more than once per month.</p>
+              </li>
+              <li>
+                <Toggle checked={communication.marketingAndSpecialOffersEmail} onChange={handleToggle('marketingAndSpecialOffersEmail')}>
+                  Marketing &amp; Special Offers
+                </Toggle>
+                <p>No more than 3-5 times per year we like to share special offers and other marketing related announcements about the great things Squeaky can do for your business.</p>
+              </li>
+              <li>
+                <Toggle checked={communication.knowledgeSharingEmail} onChange={handleToggle('knowledgeSharingEmail')}>
+                  Knowledge Sharing
+                </Toggle>
+                <p>Receive one email per quarter containing links to the top content that Squeaky has published that quarter. Topics include Product, UX, Marketing, Conversion and Customer Success.</p>
+              </li>
+            </ul>
+          </Container>
+        )}
       </Main>
     </>
   );
