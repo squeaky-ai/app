2872d1990f47d98e248d981ee28a8f324ae00fdd
Lewis Monteith <lewismonteith@gmail.com>
2022-01-16 13:25:15 +0000

    Add some basic sorting for the admin page

--- src/components/admin/sites-table.tsx
+++ src/components/admin/sites-table.tsx
@@ -1,27 +1,76 @@
 import React from 'react';
 import type { FC } from 'react';
 import { Table, Cell, Row } from 'components/table';
+import { Sort } from 'components/sort';
 import { SitesTableRow } from 'components/admin/sites-table-row';
 import type { Site } from 'types/graphql';
+import type { SitesSort } from 'types/admin';
 
 interface Props {
   sites: Site[];
 }
 
-export const SitesTable: FC<Props> = ({ sites }) => (
-  <Table className='sites-table'>
-    <Row className='head'>
-      <Cell>ID</Cell>
-      <Cell>Name</Cell>
-      <Cell>Url</Cell>
-      <Cell>Owner Name</Cell>
-      <Cell>Plan Name</Cell>
-      <Cell>Plan Exceeded</Cell>
-      <Cell>Tracking Code Status</Cell>
-      <Cell>Team Count</Cell>
-    </Row>
-    {sites.map(site => (
-      <SitesTableRow key={site.id} site={site} />
-    ))}
-  </Table>
-);
+const sortSites = (sort: SitesSort) => (a: Site, b: Site) => {
+  switch(sort) {
+    case 'name__asc':
+      return a.name.localeCompare(b.name );
+    case 'name__desc':
+      return b.name.localeCompare(a.name);
+    case 'plan_name__asc':
+      return a.plan.name.localeCompare(b.plan.name);
+    case 'plan_name__desc':
+      return b.plan.name.localeCompare(a.plan.name);
+    case 'team_count__asc':
+      return a.team.length - b.team.length;
+    case 'team_count__desc':
+      return b.team.length - a.team.length;
+  }
+};
+
+export const SitesTable: FC<Props> = ({ sites }) => {
+  const [sort, setSort] = React.useState<SitesSort>('name__asc');
+
+  const results = [...sites].sort(sortSites(sort));
+
+  return (
+    <Table className='sites-table'>
+      <Row className='head'>
+        <Cell>ID</Cell>
+        <Cell>
+          Name
+          <Sort 
+            name='name' 
+            order={sort} 
+            onAsc={() => setSort('name__asc')} 
+            onDesc={() => setSort('name__desc')} 
+          />
+        </Cell>
+        <Cell>Url</Cell>
+        <Cell>Owner Name</Cell>
+        <Cell>
+          Plan Name
+          <Sort 
+            name='plan_name' 
+            order={sort} 
+            onAsc={() => setSort('plan_name__asc')} 
+            onDesc={() => setSort('plan_name__desc')} 
+          />
+        </Cell>
+        <Cell>Plan Exceeded</Cell>
+        <Cell>Tracking Code Status</Cell>
+        <Cell>
+          Team Count
+          <Sort 
+            name='team_count' 
+            order={sort} 
+            onAsc={() => setSort('team_count__asc')} 
+            onDesc={() => setSort('team_count__desc')} 
+          />
+        </Cell>
+      </Row>
+      {results.map(site => (
+        <SitesTableRow key={site.id} site={site} />
+      ))}
+    </Table>
+  );
+};
--- src/components/admin/users-table.tsx
+++ src/components/admin/users-table.tsx
@@ -1,24 +1,73 @@
 import React from 'react';
 import type { FC } from 'react';
 import { Table, Cell, Row } from 'components/table';
+import { Sort } from 'components/sort';
 import { UsersTableRow } from 'components/admin/users-table-row';
 import type { User } from 'types/graphql';
+import type { UserSort } from 'types/admin';
 
 interface Props {
   users: User[];
 }
 
-export const UsersTable: FC<Props> = ({ users }) => (
-  <Table className='users-table'>
-    <Row className='head'>
-      <Cell>ID</Cell>
-      <Cell>Name</Cell>
-      <Cell>Email</Cell>
-      <Cell>Superuser</Cell>
-      <Cell>Created At</Cell>
-    </Row>
-    {users.map(user => (
-      <UsersTableRow key={user.id} user={user} />
-    ))}
-  </Table>
-);
+const sortUsers = (sort: UserSort) => (a: User, b: User) => {
+  switch(sort) {
+    case 'name__asc':
+      return (a.fullName || '').localeCompare(b.fullName || '');
+    case 'name__desc':
+      return (b.fullName || '').localeCompare(a.fullName || '');
+    case 'superuser__asc':
+      return a.superuser ? 1 : 0;
+    case 'superuser__desc':
+      return a.superuser ? 0 : 1;
+    case 'created_at__asc':
+      return new Date(a.createdAt).valueOf() - new Date(b.createdAt).valueOf();
+    case 'created_at__desc':
+      return new Date(b.createdAt).valueOf() - new Date(a.createdAt).valueOf();
+  }
+};
+
+export const UsersTable: FC<Props> = ({ users }) => {
+  const [sort, setSort] = React.useState<UserSort>('created_at__desc');
+
+  const results = [...users].sort(sortUsers(sort));
+
+  return (
+    <Table className='users-table'>
+      <Row className='head'>
+        <Cell>ID</Cell>
+        <Cell>
+          Name
+          <Sort 
+            name='name' 
+            order={sort} 
+            onAsc={() => setSort('name__asc')} 
+            onDesc={() => setSort('name__desc')} 
+          />
+        </Cell>
+        <Cell>Email</Cell>
+        <Cell>
+          Superuser
+          <Sort 
+            name='superuser' 
+            order={sort} 
+            onAsc={() => setSort('superuser__asc')} 
+            onDesc={() => setSort('superuser__desc')} 
+          />
+        </Cell>
+        <Cell>
+          Created At
+          <Sort 
+            name='created_at' 
+            order={sort} 
+            onAsc={() => setSort('created_at__asc')} 
+            onDesc={() => setSort('created_at__desc')} 
+          />
+        </Cell>
+      </Row>
+      {results.map(user => (
+        <UsersTableRow key={user.id} user={user} />
+      ))}
+    </Table>
+  );
+};
