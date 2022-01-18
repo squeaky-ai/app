580113924eb74927128798021c438f2c568def03
Lewis Monteith <lewismonteith@gmail.com>
2022-01-18 15:11:31 +0000

    Use the library

--- src/components/flag.tsx
+++ src/components/flag.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import type { FC } from 'react';
-import Image from 'next/image';
 
 interface Props {
   code: string;
 }
 
 export const Flag: FC<Props> = ({ code }) => (
-  <span className='flag'>
-    <Image src={`/flags/${code.toLowerCase()}.svg`} width={16} height={16} />
-  </span>
+  <span className={`fi fi-${code.toLowerCase()} flag`} />
 );
