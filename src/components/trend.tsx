65c5dbc29072c3fd58e74dc821c5f8eb7281cf79
Lewis Monteith <lewismonteith@gmail.com>
2022-01-27 15:17:34 +0000

    Add tooltip for trends

--- src/components/trend.tsx
+++ src/components/trend.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import type { FC } from 'react';
 import classnames from 'classnames';
 import { Icon } from 'components/icon';
+import { Tooltip } from 'components/tooltip';
 
 interface Props {
   direction: 'up' | 'down'
@@ -9,11 +10,15 @@ interface Props {
 }
 
 export const Trend: FC<Props> = ({ direction, value }) => (
-  <span className={classnames('trend', direction)}>
-    {direction === 'up'
-      ? <Icon name='arrow-right-up-line' />
-      : <Icon name='arrow-right-down-line' />
-    }
-    <span>{value}</span>
-  </span>
+  <Tooltip button={
+    <span className={classnames('trend', direction)}>
+      {direction === 'up'
+        ? <Icon name='arrow-right-up-line' />
+        : <Icon name='arrow-right-down-line' />
+      }
+      <span>{value}</span>
+    </span>
+  }>
+    Trend since previous period
+  </Tooltip>
 );
