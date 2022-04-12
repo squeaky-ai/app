8cf5dd7bf5f55e7882f930e0a95940c6524545e8
Lewis Monteith <lewismonteith@gmail.com>
2022-04-12 10:50:58 +0100

    Add delete button for selector

--- src/components/widget/css-selectors.tsx
+++ src/components/widget/css-selectors.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import type { FC } from 'react';
 import { cssSelectorBlacklistCreate, cssSelectorBlacklistDelete } from 'lib/api/graphql';
 import type { Site } from 'types/graphql';
+import { Button } from 'components/button';
+import { Icon } from 'components/icon';
 
 interface Props {
   site: Site;
@@ -12,15 +14,29 @@ type Message = {
   selector: string;
 }
 
+const getMessageFromEvent = (messageEvent: MessageEvent<string>): Message | null => {
+  try {
+    return JSON.parse(messageEvent.data);
+  } catch {
+    return null;
+  }
+};
+
 export const CssSelectors: FC<Props> = ({ site }) => {
   const onWindowMessage = async (messageEvent: MessageEvent<string>) => {
-    const message: Message = JSON.parse(messageEvent.data);
+    const message = getMessageFromEvent(messageEvent);
+
+    if (!message) return;
 
     const func = message.action === 'create'
       ? cssSelectorBlacklistCreate
       : cssSelectorBlacklistDelete;
 
     await func({ siteId: site.id, selector: message.selector });
+  };
+
+  const handleDeleteSelector = async (selector: string) => {
+    await cssSelectorBlacklistDelete({ siteId: site.id, selector })
   };
 
   React.useEffect(() => {
@@ -38,6 +54,9 @@ export const CssSelectors: FC<Props> = ({ site }) => {
         {site.cssSelectorBlacklist.map(selector => (
           <li key={selector}>
             {selector}
+            <Button className='delete-selector' onClick={() => handleDeleteSelector(selector)}>
+              <Icon name='close-line' />
+            </Button>
           </li>
         ))}
       </ul>
