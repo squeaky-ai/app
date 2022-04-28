19dbdb5635708ddb726ef41505309d5d42de8f04
Lewis Monteith <lewismonteith@gmail.com>
2022-04-28 11:39:01 +0100

    Fix build

--- src/components/sites/heatmaps/heatmaps.tsx
+++ src/components/sites/heatmaps/heatmaps.tsx
@@ -73,7 +73,7 @@ export const Heatmaps: FC<Props> = ({ page, pages, period, setPage, setPeriod })
           {heatmaps.recordingId && featureFlagEnabled(FeatureFlag.SHUFFLE_HEATMAPS) && (
             <div className='button-group'>
               <Tooltip portalClassName='suffle-recording-tooltip' button={<Icon name='shuffle-line' />} buttonClassName='quaternary shuffle-recording' buttonOnClick={excludeRecording}>
-                Squeaky shows you a random snapshot of your selected page from within the time period you have defined. If the snapshot is corrupted click the shuffle button and we'll find an alternative view of your page
+                Squeaky shows you a random snapshot of your selected page from within the time period you have defined. If the snapshot is corrupted click the shuffle button and we&apos;ll find an alternative view of your page
               </Tooltip>
             </div>
           )}
