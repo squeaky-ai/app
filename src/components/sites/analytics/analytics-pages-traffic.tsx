ff08984ee20284ac94831d24e1bcde06d4a71b49
Lewis Monteith <lewismonteith@gmail.com>
2022-07-06 19:47:16 +0100

    Add basic grid for pages traffic

--- src/components/sites/analytics/analytics-pages-traffic.tsx
+++ src/components/sites/analytics/analytics-pages-traffic.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import type { FC } from 'react';
+import { Icon } from 'components/icon';
+import { Card } from 'components/card';
 import type { Site } from 'types/graphql';
 import type { TimePeriod } from 'types/common';
 
@@ -12,7 +14,47 @@ interface Props {
 export const AnalyticsPagesTraffic: FC<Props> = () => {
   return  (
     <div className='analytics-traffic pages'>
+      <div className='grid-item average-time-on-page'>
+        <Card>
+          <h5>Average Time On Page</h5>
+        </Card>
+      </div>
 
+      <div className='grid-item average-visits-per-session'>
+        <Card>
+          <h5>Average Visits Per Session</h5>
+        </Card>
+      </div>
+
+      <div className='grid-item bounce-rate'>
+        <Card>
+          <h5>Bounce Rate</h5>
+        </Card>
+      </div>
+
+      <div className='grid-item exit-rate'>
+        <Card>
+          <h5>Exit Rate</h5>
+        </Card>
+      </div>
+
+      <div className='grid-item visitors-graph'>
+        <Card>
+          <h5>Visitors</h5>
+        </Card>
+      </div>
+
+      <div className='grid-item time-of-day'>
+        <Card>
+          <h5><Icon name='group-line' /> Visitors by time of day</h5>
+        </Card>
+      </div>
+
+      <div className='grid-item time-averages'>
+        <Card>
+
+        </Card>
+      </div>
     </div>
   );
 };
