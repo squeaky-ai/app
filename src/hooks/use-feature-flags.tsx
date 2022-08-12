eb360bf89a1f25db5d74ddc24784b3969ccf5131
Lewis Monteith <lewismonteith@gmail.com>
2022-08-12 09:40:53 +0100

    Add feature flag for sidebar

--- src/hooks/use-feature-flags.tsx
+++ src/hooks/use-feature-flags.tsx
@@ -19,11 +19,13 @@ type Names = Record<FeatureFlag, string>;
 export const allFeatureFlags: FeatureFlag[] = [
   FeatureFlag.MAGIC_ERASURE,
   FeatureFlag.CHEESE_LOADER,
+  FeatureFlag.PER_PAGE_ANALYTICS,
 ];
 
 export const featureFlagNames: Names = {
   MAGIC_ERASURE: 'Magic Erasure',
   CHEESE_LOADER: 'Cheese Loader',
+  PER_PAGE_ANALYTICS: 'Per Page Analytics',
 };
 
 const options: State = [
@@ -35,6 +37,12 @@ const options: State = [
   },
   {
     key: FeatureFlag.CHEESE_LOADER,
+    value: false,
+    superuser: true,
+    description: '',
+  },
+  {
+    key: FeatureFlag.PER_PAGE_ANALYTICS,
     value: false,
     superuser: true,
     description: '',
