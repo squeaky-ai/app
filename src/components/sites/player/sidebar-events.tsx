1cb7e08a5f465f353b85710a4874b3fd2513d129
Lewis Monteith <lewismonteith@gmail.com>
2022-11-14 19:07:04 +0000

    Hack hiding the inactivity

--- src/components/sites/player/sidebar-events.tsx
+++ src/components/sites/player/sidebar-events.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import type { FC } from 'react';
 import type { Replayer } from 'rrweb';
 import classnames from 'classnames';
+import { last } from 'lodash';
 import { EventIcon } from 'components/sites/events/event-icon';
 import { EventTimestamp } from 'components/sites/player/event-timestamp';
 import { SidebarErrorModal } from 'components/sites/player/sidebar-error-modal';
@@ -28,11 +29,24 @@ export const SidebarEvents: FC<Props> = ({ events, recording, state, replayer, d
     recording.inactivity
   );
 
+  const maxKnownEventTimestamp = last(events).timestamp;
+
   const getEventFromInteractionEvent = (item: InteractionEventItem): ErrorEvent | null => {
     const event = events.find(e => e.id === item.id);
     if (event?.type === CustomEvents.ERROR) return event;
 
     return null;
+  };
+
+  // Inactivity is known for the entire recording, whereas events
+  // are paginated in. If you have a super long recording then you
+  // can end up with all of the inactivity bunched up at the
+  // begining of the recording, and then it fans out as the recording
+  // goes on.
+  const shouldHideInactivityEvent = (item: InteractionEventItem): boolean => {
+    return item.eventName === 'inactivity'
+      ? item.timestampStart >= maxKnownEventTimestamp
+      : false;
   };
 
   return (
@@ -45,23 +59,23 @@ export const SidebarEvents: FC<Props> = ({ events, recording, state, replayer, d
       <ul className={classnames('datarow', { compact: state.eventOptions.includes('compact') })}>
         {interactionEvents.map((item, index) => (
           <li 
-            className={classnames('icon', item.eventName, { hidden: !item.show })} 
+            className={classnames('icon', item.eventName, { hidden: !item.show || shouldHideInactivityEvent(item) })} 
             key={`${item.timestampStart}_${index}`}
           >
-              <div className={item.eventName}>
-                <EventIcon type={item.eventName} />
-                <p className='title'>
-                  {item.label} 
-                  <span>
-                    <EventTimestamp timestamp={item.timestampStart} offset={startedAt} replayer={replayer} />
-                    {item.timestampEnd && (
-                      <> - <EventTimestamp timestamp={item.timestampEnd} offset={startedAt} replayer={replayer} /></>
-                    )}
-                  </span>
-                </p>
-                {item.info && <p className='info'>{item.info}</p>}
-                {item.eventName === 'error' && <SidebarErrorModal event={getEventFromInteractionEvent(item)} />}
-              </div>
+            <div className={item.eventName}>
+              <EventIcon type={item.eventName} />
+              <p className='title'>
+                {item.label} 
+                <span>
+                  <EventTimestamp timestamp={item.timestampStart} offset={startedAt} replayer={replayer} />
+                  {item.timestampEnd && (
+                    <> - <EventTimestamp timestamp={item.timestampEnd} offset={startedAt} replayer={replayer} /></>
+                  )}
+                </span>
+              </p>
+              {item.info && <p className='info'>{item.info}</p>}
+              {item.eventName === 'error' && <SidebarErrorModal event={getEventFromInteractionEvent(item)} />}
+            </div>
           </li>
         ))}
       </ul>
