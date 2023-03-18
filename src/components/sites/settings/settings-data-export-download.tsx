b57f3152cd7954bd84ae61ec8432b5d9030b7fa3
Lewis Monteith <lewismonteith@gmail.com>
2023-03-18 11:19:32 +0000

    Add download button

--- src/components/sites/settings/settings-data-export-download.tsx
+++ src/components/sites/settings/settings-data-export-download.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import type { FC } from 'react';
+import getConfig from 'next/config';
 import type { DataExport, Site } from 'types/graphql';
-import { Button } from 'components/button';
 
 interface Props {
   site: Site;
   dataExport: DataExport;
 }
 
-export const SettingsDataExportDownload: FC<Props> = () => (
-  <Button className='link primary'>Download</Button>
-);
+const { publicRuntimeConfig } = getConfig();
+
+export const SettingsDataExportDownload: FC<Props> = ({ dataExport, site }) => {
+  const downloadLink = `${publicRuntimeConfig.apiHost}/api/data_exports/${dataExport.id}?site_id=${site.id}`;
+
+  return (
+    <a className='button link primary' target='_blank' href={downloadLink}>
+      Download
+    </a>
+  );
+};
