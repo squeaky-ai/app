c64c3400302d606a88d2fedae3c3f617293424c6
Lewis Monteith <lewismonteith@gmail.com>
2023-05-15 20:09:51 +0100

    Don't scroll events tabs

--- src/components/sites/events/event-tabs.tsx
+++ src/components/sites/events/event-tabs.tsx
@@ -21,12 +21,12 @@ export const EventTabs: FC<Props> = ({ site, active }) => {
     <div className='event-tabs'>
       <ul className='tab-header' role='navigation' aria-label='Event stats'>
         <li className='tab'>
-          <Link href={`/sites/${site.id}/events/history/stats?${search}`} className={classnames('button tab-button', { active: active === 'stats' })}>
+          <Link scroll={false} href={`/sites/${site.id}/events/history/stats?${search}`} className={classnames('button tab-button', { active: active === 'stats' })}>
             Stats
           </Link>
         </li>
         <li className='tab'>
-          <Link href={`/sites/${site.id}/events/history/feed?${search}`} className={classnames('button tab-button', { active: active === 'feed' })}>
+          <Link scroll={false} href={`/sites/${site.id}/events/history/feed?${search}`} className={classnames('button tab-button', { active: active === 'feed' })}>
             Event Feed
           </Link>
         </li>
