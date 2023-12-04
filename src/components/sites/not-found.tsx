cfca60ecca1b9bf7cdfd9e07639d58f925ec8721
Lewis Monteith <lewismonteith@gmail.com>
2023-12-04 09:22:09 +0000

    Remove the sidebar on site 404 pages

--- src/components/sites/not-found.tsx
+++ src/components/sites/not-found.tsx
@@ -9,15 +9,22 @@ interface Props {
   className?: string;
 }
 
-export const NotFound: FC<Props> = ({ className }) => (
-  <div className={classnames('not-found', className)}>
-    <Container className='md'>
-      <Illustration illustration='illustration-10' height={256} width={256} alt='Page not found' />
-      <h2>404</h2>
-      <p>The page you are looking for cannot be found.</p>
-      <Link href='/sites' className='button primary'>
-        Back to sites
-      </Link>
-    </Container>
-  </div>
-);
+export const NotFound: FC<Props> = ({ className }) => {
+  React.useEffect(() => {
+    // This is a hack, but the page component is out of scope
+    document.querySelector('.page').classList.add('not-found');
+  }, []);
+
+  return (
+    <div className={classnames('not-found', className)}>
+      <Container className='md'>
+        <Illustration illustration='illustration-10' height={256} width={256} alt='Page not found' />
+        <h2>404</h2>
+        <p>The page you are looking for cannot be found.</p>
+        <Link href='/sites' className='button primary'>
+          Back to sites
+        </Link>
+      </Container>
+    </div>
+  );
+};
