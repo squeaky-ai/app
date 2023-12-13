32e23b349dbb64b47c2e4ea6939003191365b151
Lewis Monteith <lewismonteith@gmail.com>
2023-12-13 12:12:12 +0000

    Update dev hostnames

--- next.config.js
+++ next.config.js
@@ -2,11 +2,11 @@ const { NODE_ENV } = process.env;
 
 const IS_DEV = NODE_ENV !== 'production';
 
-const WEB_HOST_NAME = IS_DEV ? 'http://localhost:3333' : 'https://squeaky.ai';
+const WEB_HOST_NAME = IS_DEV ? 'http://squeaky.test' : 'https://squeaky.ai';
 
-const APP_HOST_NAME = IS_DEV ? 'http://localhost:3000' : 'https://app.squeaky.ai';
+const APP_HOST_NAME = IS_DEV ? 'http://app.squeaky.test' : 'https://app.squeaky.ai';
 
-const API_HOST_NAME = IS_DEV ? 'http://localhost:4000' : 'https://api.squeaky.ai';
+const API_HOST_NAME = IS_DEV ? 'http://api.squeaky.test' : 'https://api.squeaky.ai';
 
 /**
  * @type {import('next').NextConfig}
